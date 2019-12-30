e75c860fd273696262e0e2a28a8bd75aa32a1f6d
Çağdaş Kurultay Kalkan <c.kurultay@orgino.com.tr>
2019-12-31 00:36:39 +0300

    Use Multiple Contexts

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React from 'react';
 import Navbar from "./components/Navbar";
 import BookList from "./components/BookList";
 import ThemeContextProvider from "./contexts/ThemeContext";
+import AuthContextProvider from "./contexts/AuthContext";
 import ThemeToggle from "./components/ThemeToggle";
 function App() {
   return (
     <div className="App">
       <ThemeContextProvider>
+        <AuthContextProvider>
         <Navbar/>
         <BookList/>
         <ThemeToggle/>
+        </AuthContextProvider>
       </ThemeContextProvider>
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,35 @@
 import React, {Component} from 'react'
 import { ThemeContext } from "../contexts/ThemeContext";
+import { AuthContext } from "../contexts/AuthContext";
 
 
 class Navbar extends Component {
   static contextType=ThemeContext
   render() {
     return (
-      <ThemeContext.Consumer>
-        {(themeContext)=> {
-          const {isLightTheme, light, dark} = themeContext;
-          const theme = isLightTheme?light:dark;
-          return ( <nav style={{background:theme.ui, color:theme.syntax}}>
-            <h1>Context App</h1>
-            <ul>
-              <li>Home</li>
-              <li>About</li>
-              <li>Contact</li>
-            </ul>
-          </nav>)
-        }}
-      </ThemeContext.Consumer>
+      <AuthContext.Consumer>
+        {(authContext)=>(
+          <ThemeContext.Consumer>
+            {(themeContext)=> {
+              const {isLightTheme, light, dark} = themeContext;
+              const {toggleAuth,isAuthenticated} = authContext;
+              const theme = isLightTheme?light:dark;
+              return ( <nav style={{background:theme.ui, color:theme.syntax}}>
+                <h1>Context App</h1>
+                <div onClick={toggleAuth}>
+                  {isAuthenticated?'Çıkış Yap':'Giriş Yap'}
+                </div>
+                <ul>
+                  <li>Home</li>
+                  <li>About</li>
+                  <li>Contact</li>
+                </ul>
+              </nav>)
+            }}
+          </ThemeContext.Consumer>
+        )}
+      </AuthContext.Consumer>
+
     );
   }
 }
