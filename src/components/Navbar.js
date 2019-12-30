af0523b0a26cecf34fc590d708b2c48e818d90bc
Çağdaş Kurultay Kalkan <c.kurultay@orgino.com.tr>
2019-12-31 00:25:21 +0300

    context consumer example

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,17 +5,21 @@ import { ThemeContext } from "../contexts/ThemeContext";
 class Navbar extends Component {
   static contextType=ThemeContext
   render() {
-    const {isLightTheme, light, dark} = this.context;
-    const theme = isLightTheme?light:dark
     return (
-      <nav style={{background:theme.ui, color:theme.syntax}}>
-      <h1>Context App</h1>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
+      <ThemeContext.Consumer>
+        {(themeContext)=> {
+          const {isLightTheme, light, dark} = themeContext;
+          const theme = isLightTheme?light:dark;
+          return ( <nav style={{background:theme.ui, color:theme.syntax}}>
+            <h1>Context App</h1>
+            <ul>
+              <li>Home</li>
+              <li>About</li>
+              <li>Contact</li>
+            </ul>
+          </nav>)
+        }}
+      </ThemeContext.Consumer>
     );
   }
 }
