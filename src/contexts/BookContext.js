368817e9b4de8d57b3a4cdda54050a3b3a9b28c0
Çağdaş Kurultay Kalkan <c.kurultay@orgino.com.tr>
2019-12-31 16:27:06 +0300

    add and remove functions to Book Context

--- src/contexts/BookContext.js
+++ src/contexts/BookContext.js
@@ -1,14 +1,25 @@
 import React, {createContext, useState} from 'react'
-
+import uuid from 'uuid/v1'
 export const BookContext=createContext();
 
 const BookContextProvider = (props)=>{
 
   const [books, setBooks] = useState([
-    {title:'Book 1',id:1},
-    {title:'Book 2',id:2},
-    {title:'Book 3',id:3},
+    {title:'Dönüşüm', author:'Franz Kafka', id:1},
+    {title:'Dava', author:'Franz Kafka', id:2},
+    {title:'Hayvan Çiftliği', author:'George Orwell', id:3},
+    {title:'1984', author:'George Orwell', id:4},
+    {title:'Yeni Cesur Dünya', author:'Aldouse Huxley', id:5},
   ]);
+
+  const addBook=(title,author)=> {
+    setBooks([...books, {title, author,id:uuid()}])
+  };
+
+  const removeBook = (id)=> {
+    setBooks(books.filter((b)=> b.id !== id))
+  };
+
   return (
     <BookContext.Provider value={{books}}>
       {props.children}
